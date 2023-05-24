90b770380f0e028e48c80dab598e1dda413150a3
nikolay5r <nradilov30@gmail.com>
2023-05-24 21:52:10 +0300

    Added initialization and declaration to functions in IntersectionSet class

--- Sets/IntersectionSet.cpp
+++ Sets/IntersectionSet.cpp
@@ -1,1 +1,21 @@
 #include "IntersectionSet.h"
+
+IntersectionSet::IntersectionSet(const Set** sets, size_t numberOfSets) : CollectionSet(sets, numberOfSets) {}
+
+bool IntersectionSet::has(long element) const
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!sets[i]->has(element))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+Set* IntersectionSet::clone() const
+{
+	return new IntersectionSet(*this);
+}
