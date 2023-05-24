bf277a17fc54c78c008eee18ca0ff64efe6100f8
nikolay5r <nradilov30@gmail.com>
2023-05-24 21:03:47 +0300

    Added declarations to CollectionSet class

--- Sets/CollectionSet.h
+++ Sets/CollectionSet.h
@@ -3,7 +3,27 @@
 
 class CollectionSet : public Set
 {
+private:
+	void free();
+	void copyFrom(const CollectionSet& other);
+	void moveFrom(CollectionSet&& other);
 protected:
 	Set** sets = nullptr;
+	size_t size = 0;
+	size_t capacity = 0;
+
+public:
+	CollectionSet(const Set** sets);
+	CollectionSet(const CollectionSet& other);
+	CollectionSet(CollectionSet&& other) noexcept;
+
+	void addSet(const Set& set);
+	void addSet(Set&& set);
+	void removeSet(size_t index);
+
+	CollectionSet& operator=(const CollectionSet& other);
+	CollectionSet& operator=(CollectionSet&& other) noexcept;
+
+	virtual ~CollectionSet();
 };
 
