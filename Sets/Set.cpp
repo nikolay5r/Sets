0f84764c155b4a89c0b51af8ae2c54850b5a6898
nikolay5r <nradilov30@gmail.com>
2023-05-24 17:39:41 +0300

    Added Initialization for abstract class Set

--- Sets/Set.cpp
+++ Sets/Set.cpp
@@ -1,1 +1,23 @@
 #include "Set.h"
+#include "MyVector.hpp"
+#include <limits>
+
+size_t Set::size() const
+{
+	return elements.size();
+}
+
+bool Set::isEmpty() const
+{
+	return elements.isEmpty();
+}
+
+bool Set::has(long element) const
+{
+	return elements.contains(element);
+}
+
+bool Set::isElementValid(long element) const
+{
+	return element < UINT_MAX&& element > INT_MIN;
+}
