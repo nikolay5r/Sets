3da8f0affbd8f32a930fceb3b8174f13439b0961
nikolay5r <nradilov30@gmail.com>
2023-05-25 15:04:39 +0300

    Added example of how the programme works. Set.dat file is a binary file in which there is an intersection of sets that are placed in first.dat, second.dat and third.dat. Sets in first.dat, second.dat and third.dat are CriteriaSet types.

--- Sets/CriteriaSet.cpp
+++ Sets/CriteriaSet.cpp
@@ -32,31 +32,15 @@ CriteriaSet::CriteriaSet(bool (*pred)(long long elementToCheck, long long criter
 
 CriteriaSet::CriteriaSet(Criteria criteria, long long criteriaElement) : criteria(criteria)
 {
+	assignCriteriaFunction(criteria);
 	attestElement(criteriaElement);
 	criteriaElements.push_back(criteriaElement);
-}
-
-void CriteriaSet::assignCriteriaFunction(Criteria criteria)
-{
-	switch (criteria)
-	{
-	case Criteria::Equal:
-		pred = criteria::isEqualTo;
-		break;
-	case Criteria::Divisible:
-		pred = criteria::isDivisibleTo;
-		break;
-	case Criteria::NotDivisible:
-		break;
-	default:
-		pred = criteria::isNotDivisibleTo;
-		break;
-	}
 }
 
 CriteriaSet::CriteriaSet(Criteria criteria, const MyVector<long long>& criteriaElements) : criteriaElements(criteriaElements), criteria(criteria)
 {
 	size_t size = criteriaElements.size();
+	assignCriteriaFunction(criteria);
 	for (size_t i = 0; i < size; i++)
 	{
 		attestElement(criteriaElements[i]);
@@ -65,10 +49,30 @@ CriteriaSet::CriteriaSet(Criteria criteria, const MyVector<long long>& criteriaE
 
 CriteriaSet::CriteriaSet(Criteria criteria, size_t numberOfCriteriaElements, const long long* criteriaElements) : criteria(criteria)
 {
+	assignCriteriaFunction(criteria);
 	for (size_t i = 0; i < numberOfCriteriaElements; i++)
 	{
 		attestElement(criteriaElements[i]);
 		this->criteriaElements.push_back(criteriaElements[i]);
+	}
+}
+
+void CriteriaSet::assignCriteriaFunction(Criteria criteria)
+{
+	switch (criteria)
+	{
+	case Criteria::Equal:
+		pred = criteriaFunctions::isEqualTo;
+		break;
+	case Criteria::Divisible:
+		pred = criteriaFunctions::isDivisibleTo;
+		break;
+	case Criteria::NotDivisible:
+		pred = criteriaFunctions::isNotDivisibleTo;
+		break;
+	default:
+		pred = criteriaFunctions::otherFunction;
+		break;
 	}
 }
 
@@ -77,30 +81,35 @@ bool CriteriaSet::has(long long element) const
 	size_t size = criteriaElements.size();
 	for (size_t i = 0; i < size; i++)
 	{
-		if ((*pred)(element, criteriaElements[i]))
+		if ((*pred)(element, criteriaElements[i]) && criteria != Criteria::NotDivisible)
 		{
 			return true;
 		}
+		else if (!(*pred)(element, criteriaElements[i]) && criteria == Criteria::NotDivisible)
+		{
+			return false;
+		}
 	}
-	return false;
+
+	return criteria == Criteria::NotDivisible;
 }
 
-bool criteria::isEqualTo(long long elementToCheck, long long criteriaElement)
+bool criteriaFunctions::isEqualTo(long long elementToCheck, long long criteriaElement)
 {
 	return elementToCheck == criteriaElement;
 }
 
-bool criteria::isNotDivisibleTo(long long elementToCheck, long long criteriaElement)
+bool criteriaFunctions::isNotDivisibleTo(long long elementToCheck, long long criteriaElement)
 {
 	return elementToCheck % criteriaElement != 0;
 }
 
-bool criteria::isDivisibleTo(long long elementToCheck, long long criteriaElement)
+bool criteriaFunctions::isDivisibleTo(long long elementToCheck, long long criteriaElement)
 {
 	return !isNotDivisibleTo(elementToCheck, criteriaElement);
 }
 
-bool criteria::otherFunction(long long elementToCheck, long long criteriaElement)
+bool criteriaFunctions::otherFunction(long long elementToCheck, long long criteriaElement)
 {
 	return true;
 }
--- Sets/Main.cpp
+++ Sets/Main.cpp
@@ -3,7 +3,6 @@
 #include "Set.h"
 #include "CriteriaSet.h"
 #include "IntersectionSet.h"
-#include "SetCriteriaFunctions.h"
 #include "UnionSet.h"
 #include "MyString.h"
 #include "File_Error.h"
@@ -38,7 +37,7 @@ const long long* readElementsFromBinary(std::ifstream& file, size_t numberOfElem
 		throw File_Error("File with that name doesn't exist!");
 	}
 
-	long long* elements = new long long[numberOfElements];
+	long long* elements = new long long[numberOfElements] {0};
 	for (size_t i = 0; i < numberOfElements; i++)
 	{
 		file.read((char*)&elements[i], sizeof(int));
@@ -58,7 +57,8 @@ const char* readFileNameFromBinary(std::ifstream& file)
 	file.read((char*)&length, sizeof(length));
 	char* buff = new char[length + 1];
 	file.read(buff, length + 1);
-	delete[] buff;
+
+	return buff;
 }
 
 const Set* readSetFromBinary(const char* fileName)
@@ -83,19 +83,19 @@ const Set* readSetFromBinary(const char* fileName)
 	{
 	case 0:
 		elements = readElementsFromBinary(file, n);
-		set = new CriteriaSet(n, elements, criteria::isEqualTo);
+		set = new CriteriaSet(Criteria::Equal, n, elements);
 		delete[] elements;
 		break;
 
 	case 1:
 		elements = readElementsFromBinary(file, n);
-		set = new CriteriaSet(n, elements, criteria::isNotDivisibleTo);
+		set = new CriteriaSet(Criteria::NotDivisible, n, elements);
 		delete[] elements;
 		break;
 
 	case 2:
 		elements = readElementsFromBinary(file, n);
-		set = new CriteriaSet(n, elements, criteria::isDivisibleTo);
+		set = new CriteriaSet(Criteria::Divisible, n, elements);
 		delete[] elements;
 		break;
 
@@ -116,10 +116,20 @@ const Set* readSetFromBinary(const char* fileName)
 		throw File_Error("Invalid option read from file!");
 		break;
 	}
+
+	file.close();
 	return set;
 }
 
-void optionsChoice(const Set* set)
+void fileSystem(const Set*& set)
+{
+	std::cout << "Enter a file name to read a set: ";
+	MyString str;
+	std::cin >> str;
+	set = readSetFromBinary(str.c_str());
+}
+
+void optionsSystem(const Set* set)
 {
 	std::cout << "\nEnter '1' if you want to print numbers in an interval [a,b];\n"
 		<< "Enter '2' if you want to print the next number in row starting from 0;\n"
@@ -166,42 +176,75 @@ void optionsChoice(const Set* set)
 	}
 	}
 
-	optionsChoice(set);
+	optionsSystem(set);
 }
 
 void runSystem()
 {
-	const Set* set = nullptr;
+	static const Set* set = nullptr;
 	try
 	{
-		std::cout << "Enter a file name to read a set: ";
-		MyString str;
-		std::cin >> str;
-		set = readSetFromBinary(str.c_str());
+		if (!set)
+		{
+			fileSystem(set);
+		}
 
-		optionsChoice(set);
+		optionsSystem(set);
 	}
 	catch (const File_Error& error)
 	{
-		std::cerr << "FILE ERROR: " << error.what() << std::endl;
+		std::cerr << "FILE_ERROR: " << error.what() << std::endl;
+		if (set)
+		{
+			delete set;
+			set = nullptr;
+		}
 		runSystem();
 	}
 	catch (const Invalid_Option& error)
 	{
-		std::cerr << "INVALID OPTION: " << error.what() << std::endl;
-		optionsChoice(set);
+		std::cerr << "INVALID_OPTION: " << error.what() << std::endl;
+		runSystem();
 	}
-	catch (const std::invalid_argument&)
+	catch (const std::out_of_range& error)
 	{
-
+		std::cerr << "OUT_OF_RANGE: " << error.what() << std::endl;
+		runSystem();
 	}
-	catch (const std::exception&)
+	catch (const std::logic_error& error)
 	{
-
+		std::cerr << "LOGIC_ERROR: " << error.what() << std::endl;
+		runSystem();
+	}
+	catch (const std::invalid_argument& error)
+	{
+		std::cerr << "INVALID_ARGUMENT: " << error.what() << std::endl;
+		if (set)
+		{
+			delete set;
+			set = nullptr;
+		}
+		runSystem();
+	}
+	catch (const std::exception& error)
+	{
+		std::cerr << "ERROR: " << error.what() << " No Handling for this exception right now!" << std::endl;
+		if (set)
+		{
+			delete set;
+			set = nullptr;
+		}
+		runSystem();
 	}
 	catch (...)
 	{
 		std::cerr << "ERROR: Something went wrong! Sorry!";
+	}
+
+	if (set)
+	{
+		delete set;
+		set = nullptr;
 	}
 }
 
