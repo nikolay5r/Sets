3e34a6dc7694eeef82a04d83073e028647dc945d
nikolay5r <nradilov30@gmail.com>
2023-05-25 15:04:39 +0300

    Added example of how the programme works. Set.dat file is a binary file in which there is an intersection of sets that are placed in first.dat, second.dat and third.dat. Sets in first.dat, second.dat and third.dat are CriteriaSet types.

--- Sets/CriteriaSet.h
+++ Sets/CriteriaSet.h
@@ -34,7 +34,7 @@ public:
 	~CriteriaSet() = default;
 };
 
-namespace criteria
+namespace criteriaFunctions
 {
 	bool isEqualTo(long long elementToCheck, long long criteriaElement);
 	bool isNotDivisibleTo(long long elementToCheck, long long criteriaElement);
